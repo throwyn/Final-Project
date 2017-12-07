44d757a1e14569b62fb6209ab482f7b4aada3c52
Damian Kołodziej <damian.373@gmail.com>
2017-12-07 18:48:58 +0000

    almost working countries and teams

--- js.js
+++ js.js
@@ -1,37 +1,81 @@
-﻿define(['knockout'], function (ko) {
-    var vm = function () {
-        console.log('ViewModel initiated...')
-        //---Variáveis locais
-        var self = this;
-        var baseUri = 'http://192.168.160.28/football/api/countries';
-        self.className = 'Countries';
-        self.description = 'This page aims to demonstrate the use of the football web API for countries and the interconnection with other entities.<br > Called method(s): <ul><li>' +baseUri + '</li></ul>';
-        self.error = ko.observable();
-        self.countries = ko.observableArray([]);
-        //--- Internal functions
-        function ajaxHelper(uri, method, data) {
-            self.error(''); // Clear error message
-            return $.ajax({
-                type: method,
-                url: uri,
-                dataType: 'json',
-                contentType: 'application/json',
-                data: data ? JSON.stringify(data) : null,
-                error: function (jqXHR, textStatus, errorThrown) {
-                    console.log("AJAX Call[" + uri + "] Fail...");
-                    self.error(errorThrown);
-                }
-            })
+﻿
+//--- Internal functions
+function ajaxHelper(uri, method, data) {
+    self.error(''); // Clear error message
+    return $.ajax({
+        type: method,
+        url: uri,
+        dataType: 'json',
+        contentType: 'application/json',
+        data: data ? JSON.stringify(data) : null,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("AJAX Call[" + uri + "] Fail...");
+            self.error(errorThrown);
         }
-        //--- Externel functions (accessible outside)
-        getCountries = function () {
-            console.log('CALL: getCountries...')
-            ajaxHelper(baseUri, 'GET').done(function (data) {
-                self.countries(data);
-            });
-        };
-        //---- initial call
-        getCountries();
-    };
-    return vm;
+    });
+}
+
+$(document).ready(function () {
+    
+    $(window).on("hashchange", function () {
+        var hash = window.location.hash;
+        if (hash !== "") {
+            console.log(window.location.hash);
+            $(".pages").hide();
+            $(hash).show();
+        }
+    });
+    $(window).on("hashchange", function () {
+
+        if (window.location.hash === "#leagues") {
+            var leagues = function () {
+                console.log('ViewModel initiated...');
+                //---Variáveis locais
+                var self = this;
+                var baseUri = 'http://192.168.160.28/football/api/leagues';
+                self.className = 'Leagues';
+                self.description = 'This page aims to demonstrate the use of the football web API for countries and the interconnection with other entities.<br > Called method(s): <ul><li>' + baseUri + '</li></ul>';
+                self.error = ko.observable();
+                self.leagues = ko.observableArray([]);
+                //--- Externel functions (accessible outside)
+                self.getLeagues = function () {
+                    console.log('CALL: getLeagues...');
+                    ajaxHelper(baseUri, 'GET').done(function (data) {
+                        self.leagues(data);
+                    });
+                };
+                //---- initial call
+                self.getLeagues();
+            };
+            ko.applyBindings(leagues);
+            ko.cleanNode(leagues);
+        }
+        if (window.location.hash === "#countries") {
+            var vm = function () {
+                console.log('ViewModel initiated...');
+                //---Variáveis locais
+                var self = this;
+                var baseUri = 'http://192.168.160.28/football/api/countries';
+                self.className = 'Countries';
+                self.description = 'This page aims to demonstrate the use of the football web API for countries and the interconnection with other entities.<br > Called method(s): <ul><li>' + baseUri + '</li></ul>';
+                self.error = ko.observable();
+                self.countries = ko.observableArray([]);
+                //--- Externel functions (accessible outside)
+                self.getCountries = function () {
+                    console.log('CALL: getCountries...');
+                    ajaxHelper(baseUri, 'GET').done(function (data) {
+                        self.countries(data);
+                    });
+                };
+                //---- initial call
+                self.getCountries();
+            };
+            ko.applyBindings(vm);
+            ko.cleanNode(vm);
+        }
+    });
+});
+
+$(function () {
+
 });
